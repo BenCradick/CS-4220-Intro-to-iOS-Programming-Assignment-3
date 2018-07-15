be87b2fbb34490bf04878c57485857622f62b5ca
Ben Cradick <ben.cradick@gmail.com>
2018-07-15 14:38:36 -0500

    Started making the controller and did most of the storyboard

--- Assignment3/FormViewController.swift
+++ Assignment3/FormViewController.swift
@@ -5,7 +5,14 @@ class FormViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailAddressField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var phoneNumberField: UITextField!
+    @IBOutlet weak var nameConfirmation: UIImageView!
+    @IBOutlet weak var emailConfirmation: UIImageView!
+    @IBOutlet weak var passwordConfirmation: UIImageView!
+    @IBOutlet weak var phoneNumberConfirmation: UIImageView!
 }
 
 
